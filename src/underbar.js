c1048062b7b20a192b7f4ee4ec901369d16f161d
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-04-18 18:12:45 -0700

    Implement _.uniq()

--- src/underbar.js
+++ src/underbar.js
@@ -102,6 +102,13 @@ var _ = {};
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+	  var unique = [];
+	  _.each(array, function(item) {
+		  if (_.indexOf(unique, item) === -1) {
+			  unique.push(item)
+		  }
+	  });
+	  return unique;
   };
 
 
